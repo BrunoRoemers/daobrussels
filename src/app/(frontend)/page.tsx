176e793f1550debe48078a3a86698e7eb0e94eab
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-03-22 22:54:28 +0100

    remove pages, posts and categories collection, remove seed endpoint

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,3 @@
-import PageTemplate, { generateMetadata } from './[slug]/page'
-
-export default PageTemplate
-
-export { generateMetadata }
+export default () => {
+  return <div className="container pb-8">hello world</div>
+}
