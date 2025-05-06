9c861025d25afbbefa98dae801b02d49aadd395e
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-05-07 00:58:21 +0200

    Simplify footer

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -11,16 +11,10 @@ export async function Footer() {
   const navItems = footer?.navItems || [];
 
   return (
-    <footer className="border-t border-border bg-black dark:bg-card text-white">
+    <footer>
       <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
         <Link className="flex items-center" href="/">
-          <picture>
-            <img
-              alt="Payload Logo"
-              className="max-w-[6rem] invert-0"
-              src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/payload/src/admin/assets/images/payload-logo-light.svg"
-            />
-          </picture>
+          DAO Brussels
         </Link>
 
         <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
