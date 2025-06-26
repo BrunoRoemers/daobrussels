f0ece1d3dfe01559392d0e14dd340412b3ce967c
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-06-26 22:32:24 +0200

    #16: /events: show events

--- src/app/(frontend)/events/page.tsx
+++ src/app/(frontend)/events/page.tsx
@@ -1,7 +1,34 @@
+import EventService from '@/collections/Events/service';
+import { EventCard } from '@/components/events/event-card';
+import { Card, CardContent } from '@/components/ui/card';
+import { findDraftsOrPublicDocs } from '@/utilities/draft-mode/find-drafts-or-public-docs';
 import type { Metadata } from 'next/types';
 
 export default async function Page() {
-  // TODO
+  const data = await findDraftsOrPublicDocs({
+    collection: 'events',
+    limit: 10,
+    sort: '-date',
+  });
+
+  const events = data.docs.map((event) => new EventService(event));
+
+  return (
+    <div className="container py-8">
+      <h2 className="text-2xl font-bold mb-4">All Events</h2>
+      <div className="grid gap-4">
+        {events.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+        {events.length === 0 && (
+          <Card>
+            <CardContent>No events found</CardContent>
+          </Card>
+        )}
+        {/* TODO pagination  */}
+      </div>
+    </div>
+  );
 }
 
 export function generateMetadata(): Metadata {
