00a7698a3f244189b1d9e276ef570d7ba9dfb442
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-07-13 19:09:41 +0200

    Create migration

---
+++ src/migrations/20250713_170606_simplify_media_collection.ts
@@ -0,0 +1,116 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   CREATE TABLE "users_sessions" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"created_at" timestamp(3) with time zone,
+  	"expires_at" timestamp(3) with time zone NOT NULL
+  );
+  
+  DROP INDEX "media_sizes_thumbnail_sizes_thumbnail_filename_idx";
+  DROP INDEX "media_sizes_square_sizes_square_filename_idx";
+  DROP INDEX "media_sizes_small_sizes_small_filename_idx";
+  DROP INDEX "media_sizes_medium_sizes_medium_filename_idx";
+  DROP INDEX "media_sizes_large_sizes_large_filename_idx";
+  DROP INDEX "media_sizes_xlarge_sizes_xlarge_filename_idx";
+  DROP INDEX "redirects_from_idx";
+  ALTER TABLE "forms_emails" ALTER COLUMN "subject" SET DEFAULT 'You''ve received a new message.';
+  ALTER TABLE "media" ADD COLUMN "prefix" varchar DEFAULT 'collections/media';
+  ALTER TABLE "forms_blocks_select" ADD COLUMN "placeholder" varchar;
+  ALTER TABLE "users_sessions" ADD CONSTRAINT "users_sessions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "users_sessions_order_idx" ON "users_sessions" USING btree ("_order");
+  CREATE INDEX "users_sessions_parent_id_idx" ON "users_sessions" USING btree ("_parent_id");
+  CREATE UNIQUE INDEX "redirects_from_idx" ON "redirects" USING btree ("from");
+  ALTER TABLE "media" DROP COLUMN "sizes_thumbnail_url";
+  ALTER TABLE "media" DROP COLUMN "sizes_thumbnail_width";
+  ALTER TABLE "media" DROP COLUMN "sizes_thumbnail_height";
+  ALTER TABLE "media" DROP COLUMN "sizes_thumbnail_mime_type";
+  ALTER TABLE "media" DROP COLUMN "sizes_thumbnail_filesize";
+  ALTER TABLE "media" DROP COLUMN "sizes_thumbnail_filename";
+  ALTER TABLE "media" DROP COLUMN "sizes_square_url";
+  ALTER TABLE "media" DROP COLUMN "sizes_square_width";
+  ALTER TABLE "media" DROP COLUMN "sizes_square_height";
+  ALTER TABLE "media" DROP COLUMN "sizes_square_mime_type";
+  ALTER TABLE "media" DROP COLUMN "sizes_square_filesize";
+  ALTER TABLE "media" DROP COLUMN "sizes_square_filename";
+  ALTER TABLE "media" DROP COLUMN "sizes_small_url";
+  ALTER TABLE "media" DROP COLUMN "sizes_small_width";
+  ALTER TABLE "media" DROP COLUMN "sizes_small_height";
+  ALTER TABLE "media" DROP COLUMN "sizes_small_mime_type";
+  ALTER TABLE "media" DROP COLUMN "sizes_small_filesize";
+  ALTER TABLE "media" DROP COLUMN "sizes_small_filename";
+  ALTER TABLE "media" DROP COLUMN "sizes_medium_url";
+  ALTER TABLE "media" DROP COLUMN "sizes_medium_width";
+  ALTER TABLE "media" DROP COLUMN "sizes_medium_height";
+  ALTER TABLE "media" DROP COLUMN "sizes_medium_mime_type";
+  ALTER TABLE "media" DROP COLUMN "sizes_medium_filesize";
+  ALTER TABLE "media" DROP COLUMN "sizes_medium_filename";
+  ALTER TABLE "media" DROP COLUMN "sizes_large_url";
+  ALTER TABLE "media" DROP COLUMN "sizes_large_width";
+  ALTER TABLE "media" DROP COLUMN "sizes_large_height";
+  ALTER TABLE "media" DROP COLUMN "sizes_large_mime_type";
+  ALTER TABLE "media" DROP COLUMN "sizes_large_filesize";
+  ALTER TABLE "media" DROP COLUMN "sizes_large_filename";
+  ALTER TABLE "media" DROP COLUMN "sizes_xlarge_url";
+  ALTER TABLE "media" DROP COLUMN "sizes_xlarge_width";
+  ALTER TABLE "media" DROP COLUMN "sizes_xlarge_height";
+  ALTER TABLE "media" DROP COLUMN "sizes_xlarge_mime_type";
+  ALTER TABLE "media" DROP COLUMN "sizes_xlarge_filesize";
+  ALTER TABLE "media" DROP COLUMN "sizes_xlarge_filename";`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "users_sessions" DISABLE ROW LEVEL SECURITY;
+  DROP TABLE "users_sessions" CASCADE;
+  DROP INDEX "redirects_from_idx";
+  ALTER TABLE "forms_emails" ALTER COLUMN "subject" SET DEFAULT 'You''''ve received a new message.';
+  ALTER TABLE "media" ADD COLUMN "sizes_thumbnail_url" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_thumbnail_width" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_thumbnail_height" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_thumbnail_mime_type" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_thumbnail_filesize" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_thumbnail_filename" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_square_url" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_square_width" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_square_height" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_square_mime_type" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_square_filesize" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_square_filename" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_small_url" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_small_width" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_small_height" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_small_mime_type" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_small_filesize" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_small_filename" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_medium_url" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_medium_width" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_medium_height" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_medium_mime_type" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_medium_filesize" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_medium_filename" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_large_url" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_large_width" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_large_height" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_large_mime_type" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_large_filesize" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_large_filename" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_xlarge_url" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_xlarge_width" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_xlarge_height" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_xlarge_mime_type" varchar;
+  ALTER TABLE "media" ADD COLUMN "sizes_xlarge_filesize" numeric;
+  ALTER TABLE "media" ADD COLUMN "sizes_xlarge_filename" varchar;
+  CREATE INDEX "media_sizes_thumbnail_sizes_thumbnail_filename_idx" ON "media" USING btree ("sizes_thumbnail_filename");
+  CREATE INDEX "media_sizes_square_sizes_square_filename_idx" ON "media" USING btree ("sizes_square_filename");
+  CREATE INDEX "media_sizes_small_sizes_small_filename_idx" ON "media" USING btree ("sizes_small_filename");
+  CREATE INDEX "media_sizes_medium_sizes_medium_filename_idx" ON "media" USING btree ("sizes_medium_filename");
+  CREATE INDEX "media_sizes_large_sizes_large_filename_idx" ON "media" USING btree ("sizes_large_filename");
+  CREATE INDEX "media_sizes_xlarge_sizes_xlarge_filename_idx" ON "media" USING btree ("sizes_xlarge_filename");
+  CREATE INDEX "redirects_from_idx" ON "redirects" USING btree ("from");
+  ALTER TABLE "media" DROP COLUMN "prefix";
+  ALTER TABLE "forms_blocks_select" DROP COLUMN "placeholder";`)
+}
