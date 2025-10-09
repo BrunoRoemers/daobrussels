8d8a476d18d1d0d316fe10f02e9eda4671051f9b
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-10-10 00:00:58 +0200

    27: show selected img

--- src/features/media/features/upload-images/file-dropzone.tsx
+++ src/features/media/features/upload-images/file-dropzone.tsx
@@ -9,28 +9,38 @@ import { useId, useState, type ChangeEventHandler, type DragEventHandler } from 
 export const FileDropzone = () => {
   const id = useId();
   const [isDragging, setIsDragging] = useState(false);
+  const [files, setFiles] = useState<File[]>([]);
 
   const onDragEnter: DragEventHandler = (e) => {
-    if (e.currentTarget === e.relatedTarget) return;
+    if (e.target === e.relatedTarget) return;
     setIsDragging(true);
   };
 
   const onDragLeave: DragEventHandler = (e) => {
-    if (e.currentTarget === e.relatedTarget) return;
+    if (e.target === e.relatedTarget) return;
     setIsDragging(false);
   };
 
   const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setIsDragging(false);
+    // TODO start upload + handle multiple files
+    setFiles(Array.from(e.target.files || []));
   };
 
   return (
     <div className={cn('grid', isDragging && 'bg-green-50')}>
       <div className="col-span-full row-span-full place-self-center">
-        <CloudUpload size={40} className="mx-auto mb-2" />
-        <LabelPrimitive.Root htmlFor={id}>
-          <b>Click to upload</b> or drag and drop
-        </LabelPrimitive.Root>
+        {files.length < 1 && (
+          <>
+            <CloudUpload size={40} className="mx-auto mb-2" />
+            <LabelPrimitive.Root htmlFor={id}>
+              <b>Click to upload</b> or drag and drop
+            </LabelPrimitive.Root>
+          </>
+        )}
+        {files.map((file, index) => (
+          <img key={index} src={URL.createObjectURL(file)} alt={file.name} />
+        ))}
       </div>
       <Input
         id={id}
