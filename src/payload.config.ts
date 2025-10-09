6fc644a4ef0b9f407feaf493842e40abea3945c5
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-10-09 16:55:02 +0200

    46: Fix

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -69,7 +69,7 @@ export default buildConfig({
   },
   cors: deploymentUrls,
   plugins: [GoogleCloudStorage, Search],
-  secret: process.env.PAYLOAD_SECRET,
+  secret: process.env.PAYLOAD_SECRET!,
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
