065d60164a0a10be7de893414086b9570d6b2b81
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-10-09 16:00:43 +0200

    Remove unused font

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,9 +1,6 @@
 import { AdminBar } from '@/components/admin/admin-bar';
 import { LivePreviewListener } from '@/components/admin/live-preview-listener';
 import { ThemeToggle } from '@/components/theme-toggle';
-import { cn } from '@/utils/cn';
-import { GeistMono } from 'geist/font/mono';
-import { GeistSans } from 'geist/font/sans';
 import { ThemeProvider } from 'next-themes';
 import React from 'react';
 
@@ -11,7 +8,7 @@ import './globals.css';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
