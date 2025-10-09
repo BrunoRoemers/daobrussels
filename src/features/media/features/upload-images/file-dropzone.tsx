87588abbf7d6313502ae53d4857afb4eddfa2f08
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-10-09 22:25:35 +0200

    27: Create file drop zone

---
+++ src/features/media/features/upload-images/file-dropzone.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Input } from '@/components/ui/input';
+import { cn } from '@/utils/cn';
+import { useState, type ChangeEventHandler, type DragEventHandler } from 'react';
+
+export const FileDropzone = () => {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const onDragEnter: DragEventHandler = (e) => {
+    if (e.currentTarget === e.relatedTarget) return;
+    setIsDragging(true);
+  };
+
+  const onDragLeave: DragEventHandler = (e) => {
+    if (e.currentTarget === e.relatedTarget) return;
+    setIsDragging(false);
+  };
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setIsDragging(false);
+  };
+
+  return (
+    <Input
+      type="file"
+      multiple
+      className={cn('h-60 border-none', isDragging && 'bg-green-50')}
+      onDragEnter={onDragEnter}
+      onDragLeave={onDragLeave}
+      onChange={onChange}
+    />
+  );
+};
