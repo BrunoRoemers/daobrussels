6b723593b6d31de6234e86176e836d56ca200ac9
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-10-11 17:36:23 +0200

    27: WIP: upload in progress page

--- src/features/media/features/upload-images/upload-image-dialog.tsx
+++ src/features/media/features/upload-images/upload-image-dialog.tsx
@@ -7,6 +7,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
+import { Separator } from '@/components/ui/separator';
+import type { DialogContentProps } from '@radix-ui/react-dialog';
+import { useState } from 'react';
 import { DropzoneCarousel } from './dropzone-carousel';
 
 interface Props {
@@ -14,18 +17,40 @@ interface Props {
 }
 
 export const UploadImageDialog = ({ button }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const handleDialogClose: DialogContentProps['onInteractOutside'] = (e) => {
+    // TODO use e.preventDefault(); while the upload is in progress
+    setFiles([]);
+  };
+
   const handleUpload = (files: File[]) => {
-    console.log('handleUpload', files);
+    setFiles(files);
   };
 
   return (
     <Dialog>
       <DialogTrigger asChild>{button}</DialogTrigger>
-      <DialogContent showCloseButton={false} aria-describedby={undefined} className="p-0">
+      <DialogContent
+        aria-describedby={undefined}
+        className="p-0"
+        showCloseButton={false}
+        onInteractOutside={handleDialogClose}
+      >
         <DialogHeader className="sr-only">
           <DialogTitle>Upload images</DialogTitle>
         </DialogHeader>
-        <DropzoneCarousel className="h-72" handleUpload={handleUpload} />
+        {files.length <= 0 ? (
+          <DropzoneCarousel className="h-72" handleUpload={handleUpload} />
+        ) : (
+          <div className="min-h-72 p-4">
+            <span>todo upload status</span>
+            <Separator />
+            <span>
+              todo <b>get credit</b> for your contribution
+            </span>
+          </div>
+        )}
       </DialogContent>
     </Dialog>
   );
