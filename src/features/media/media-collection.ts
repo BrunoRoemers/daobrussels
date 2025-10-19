9cad1f46ab43d6b69cd27b63e1f969367d597898
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-10-19 14:20:11 +0200

    27: make url constant

--- src/features/media/media-collection.ts
+++ src/features/media/media-collection.ts
@@ -25,3 +25,4 @@ export const Media: CollectionConfig = {
 };
 
 export const mediaGcsPrefix = `collections/${Media.slug}`;
+export const mediaCrowdsourcedGcsPrefix = `${mediaGcsPrefix}/crowdsourced`;
