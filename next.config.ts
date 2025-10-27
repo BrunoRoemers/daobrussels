437522582f54484c600400cb1df7f5a0797b7a71
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-10-27 19:47:51 +0100

    63: undo

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.devtool = 'source-map';
-    }
-    return config;
-  },
 };
 
 export default withPayload(nextConfig);
