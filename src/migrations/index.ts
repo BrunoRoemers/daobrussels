238535dd4fd008c2596e7e798381c17e49ce8f59
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-11-04 23:23:38 +0100

    69: Migrate

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250802_154157_baseline from './20250802_154157_baseline';
 import * as migration_20251026_155604_support_frontend_media_uploads from './20251026_155604_support_frontend_media_uploads';
+import * as migration_20251104_222137_update_pods_at_events_title from './20251104_222137_update_pods_at_events_title';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20251026_155604_support_frontend_media_uploads.up,
     down: migration_20251026_155604_support_frontend_media_uploads.down,
-    name: '20251026_155604_support_frontend_media_uploads'
+    name: '20251026_155604_support_frontend_media_uploads',
+  },
+  {
+    up: migration_20251104_222137_update_pods_at_events_title.up,
+    down: migration_20251104_222137_update_pods_at_events_title.down,
+    name: '20251104_222137_update_pods_at_events_title'
   },
 ];
