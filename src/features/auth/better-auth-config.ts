e29b597be3a23b26b447e2619023443a9ea61ed7
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-11-22 11:22:45 +0100

    72: Use postgres for better auth

--- src/features/auth/better-auth-config.ts
+++ src/features/auth/better-auth-config.ts
@@ -1,12 +1,29 @@
+import { Pool as NeonPool } from '@neondatabase/serverless';
 import { betterAuth } from 'better-auth';
 import { magicLink } from 'better-auth/plugins';
-import Database from 'better-sqlite3';
+import { Pool, type PoolConfig } from 'pg';
+
+const getDatabase = (): Pool => {
+  const dbUrl = new URL(process.env.POSTGRES_URL ?? '');
+
+  // Use the `auth` schema instead of the default `public` schema to avoid conflicts with Payload CMS.
+  dbUrl.searchParams.set('options', '-c search_path=auth');
+
+  const poolConfig: PoolConfig = {
+    connectionString: dbUrl.toString(),
+  };
+
+  // Switch the database adapter based on the connection string,
+  // like Payload's `vercelPostgresAdapter` does internally.
+  return ['127.0.0.1', 'localhost'].includes(dbUrl.hostname)
+    ? new Pool(poolConfig)
+    : new NeonPool(poolConfig);
+};
 
 export const auth = betterAuth({
   // TODO baseURL from Payload
 
-  // TODO move to Payload?
-  database: new Database('better-auth.db'),
+  database: getDatabase(),
 
   emailAndPassword: {
     enabled: true,
