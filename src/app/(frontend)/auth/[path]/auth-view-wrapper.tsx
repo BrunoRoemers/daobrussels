cd95d396349d2c2ab263b9adc55e8bbee5a262c4
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2026-01-05 23:02:55 +0100

    Prerender auth view with homepage redirects, and populate the callback url client side
    useSearchParams() caused https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- src/app/(frontend)/auth/[path]/auth-view-wrapper.tsx
+++ src/app/(frontend)/auth/[path]/auth-view-wrapper.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import { useWindow } from '@/features/shared/react-hooks/use-window';
 import { AuthView } from '@daveyplate/better-auth-ui';
-import { useSearchParams } from 'next/navigation';
 
 export const AuthViewWrapper = ({ path }: { path: string }) => {
-  const searchParams = useSearchParams();
+  const window = useWindow();
+  const searchParams = new URLSearchParams(window?.location.search);
   const redirectTo = searchParams.get('redirectTo') ?? searchParams.get('redirect') ?? '/';
 
   return <AuthView path={path} redirectTo={redirectTo} />;
