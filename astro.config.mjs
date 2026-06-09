4fb1a80f59064d01964c47c93fa902b9da48038a
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2026-06-09 22:39:50 +0200

    Refine url selection

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,20 @@ import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+const getSiteUrl = () => {
+  if (process.env.VERCEL_ENV === 'production' && process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return 'https://dao.brussels';
+}
+
 export default defineConfig({
-  site: process.env.VERCEL_URL && process.env.VERCEL_ENV !== 'production' ? `https://${process.env.VERCEL_URL}` : 'https://dao.brussels',
+  site: getSiteUrl(),
   integrations: [svelte(), mdx(), sitemap(), tailwind()],
   output: 'static',
 });
