58d517e5f59ce876fbff022861ad8de8f88dd9b9
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2026-06-09 22:33:44 +0200

    Use normal url in prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://dao.brussels',
+  site: process.env.VERCEL_URL && process.env.VERCEL_ENV !== 'production' ? `https://${process.env.VERCEL_URL}` : 'https://dao.brussels',
   integrations: [svelte(), mdx(), sitemap(), tailwind()],
   output: 'static',
 });
